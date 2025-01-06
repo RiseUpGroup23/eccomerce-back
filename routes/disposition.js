12642d179d869a2ad3474724e9b3055d8fd4b8fc
Nicolás Amicone <nicolas@pocimadigital.com>
2025-01-06 13:10:48 -0300

    ajuste

--- routes/disposition.js
+++ routes/disposition.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const DispositionModel = require('../models/disposition/dispositionModel');
+const { DispositionModel } = require('../models/disposition/dispositionModel');
 
 const router = express.Router();
 
