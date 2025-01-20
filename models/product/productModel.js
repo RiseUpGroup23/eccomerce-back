6a4cf9693387bec026b7d7f79632c579d9e56db0
Nicolás Amicone <nicolas@pocimadigital.com>
2025-01-20 12:51:24 -0300

    put

--- models/product/productModel.js
+++ models/product/productModel.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const productoSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
