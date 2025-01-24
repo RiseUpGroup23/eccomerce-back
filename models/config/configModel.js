38e43a482ce67aea406ae23a58f33fa270249a2c
Nicolás Amicone <nicolas@pocimadigital.com>
2025-01-24 11:06:05 -0300

    ajuste

--- models/config/configModel.js
+++ models/config/configModel.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const ConfigSchema = new Schema({
   shopName: {
     type: String
-  }
+  },
+  shopColors: {
+    type: Map,
+    of: String
+  },
 });
 
 const ConfigModel = mongoose.model("ConfigSchema", ConfigSchema);
