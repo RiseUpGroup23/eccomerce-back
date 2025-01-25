4565ee8cd96a635dc2a259c2da83b9a99f4e1d33
Nicolás Amicone <nicolas@pocimadigital.com>
2025-01-24 21:37:54 -0300

    orders

--- models/pickup/pickupModel.js
+++ models/pickup/pickupModel.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const pickupSchema = new mongoose.Schema({
-    address: { type: String, required: true },
-    workingHours: { type: String, required: true }, 
-    contact: { type: String, required: true }, 
+    id: { type: String, required: true },  // ID para identificar la sucursal
+    name: { type: String, required: true },  // Nombre de la sucursal
+    availability: { type: String, required: false },  // Disponibilidad de la sucursal
+    address: { type: String, required: true },  // Dirección de la sucursal
+    contact: { type: String, required: false },  // Información de contacto de la sucursal
 }, { timestamps: true });
 
 module.exports = mongoose.model('Pickup', pickupSchema);
