9eb534d810197a318e3c7b1e0dff66cedf178c61
Nicolás Amicone <nicolas@pocimadigital.com>
2025-02-07 11:47:50 -0300

    login basico

--- models/user/userModel.js
+++ models/user/userModel.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
     phone: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }, // Role management
+    role: { type: String, enum: ['user', 'admin', 'superadmin'], default: 'user' }, // Role management
 }, { timestamps: true });
+
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+    }
+    next();
+});
 
 module.exports = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user/userModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -72,5 +73,73 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+//LOGIN
+
+const secretKey = "5b4a3d9c2a1c8924b9a1019258788c32561908c35745f7f10f59b7e3f3d5a1a0"
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(200).json({ logged: false, message: "Usuario no encontrado" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(200).json({ logged: false, message: "Usuario o contraseña incorrectos" });
+        }
+
+        const token = jwt.sign({ email }, secretKey, { expiresIn: '1h' });
+        res.status(200).json({ logged: true, message: "Login exitoso", token });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send();
+    }
+});
+
+router.post("/newUser", async (req, res) => {
+    try {
+        let existingUser = await User.findOne({ email: req.body.email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "El usuario ya existe" });
+        }
+
+        const newUser = new User(req.body);
+
+        await newUser.save();
+
+        res.status(201).json({ message: "Usuario creado exitosamente" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Error en endpoint" });
+    }
+});
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(403).json({ error: 'Token no proporcionado' });
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Token inválido' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+router.get('/jwt', verifyToken, (req, res) => {
+    res.json({ logged: true });
+});
+
 
 module.exports = router;
