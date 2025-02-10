ed0e323625075538211f79070695a8043581c593
Nicolás Amicone <nicolas@pocimadigital.com>
2025-02-10 16:01:35 -0300

    default Deliv

--- models/pickup/pickupModel.js
+++ models/pickup/pickupModel.js
@@ -5,6 +5,7 @@ const pickupSchema = new mongoose.Schema({
     availability: { type: String, required: false },  // Disponibilidad de la sucursal
     address: { type: String, required: true },  // Dirección de la sucursal
     contact: { type: String, required: false },  // Información de contacto de la sucursal
+    defaultDelivery: { type: Boolean, required: true, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Pickup', pickupSchema);
