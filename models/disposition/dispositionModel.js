b190aa4487bac0b346a83b1265347db02b1a6a6c
Nicolás Amicone <nicolas@pocimadigital.com>
2025-03-31 13:53:47 -0300

    valores default

--- models/disposition/dispositionModel.js
+++ models/disposition/dispositionModel.js
@@ -14,20 +14,23 @@ const DispositionItemSchema = new Schema({
 const DispositionSchema = new Schema({
     items: {
         type: [DispositionItemSchema],
-        default: () => ([
-            {
-                title: "Demo banner",
-                type: "largeSlider",
-                content: [
-                    {
-                        "desktop": "https://static.vecteezy.com/system/resources/previews/006/828/785/non_2x/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-pink-backgroud-for-banner-market-ecommerce-women-concept-free-vector.jpg",
-                        "mobile": "https://img.pikbest.com/backgrounds/20210618/creative-technology-smart-style-mobile-promotion-banner-template_6021593.jpg!bw700"
-                    }
-                ]
-            }
-        ])
+        default: function () {
+            return [
+                {
+                    title: "Demo banner",
+                    type: "largeSlider",
+                    content: [
+                        {
+                            "desktop": "https://static.vecteezy.com/system/resources/previews/006/828/785/non_2x/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-pink-backgroud-for-banner-market-ecommerce-women-concept-free-vector.jpg",
+                            "mobile": "https://img.pikbest.com/backgrounds/20210618/creative-technology-smart-style-mobile-promotion-banner-template_6021593.jpg!bw700"
+                        }
+                    ]
+                }
+            ];
+        }
     }
 }, { timestamps: true });
+
 
 // Modelo para DispositionModel
 const DispositionModel = mongoose.model("Disposition", DispositionSchema);
