26061b6e58c3193240fd95a58d0914415ee7289a
Nicolás Amicone <nicolas@pocimadigital.com>
2025-03-31 14:00:20 -0300

    valores default

--- models/disposition/dispositionModel.js
+++ models/disposition/dispositionModel.js
@@ -6,7 +6,10 @@ const DispositionItemSchema = new Schema({
     title: { type: String, required: true },
     type: { type: String, enum: ['default', 'sliderProducts', 'largeSlider', 'bannersGrid'], required: true },
     content: {
-        type: Schema.Types.Mixed, // Permite almacenar distintos tipos de contenido sin validación
+        type: Schema.Types.Mixed, // Permite almacenar distintos tipos de contenido (array, objeto, string, etc.)
+        default: function () {
+            return []; // Valor por defecto como array vacío para evitar problemas
+        }
     }
 }, { timestamps: true });
 
@@ -21,8 +24,8 @@ const DispositionSchema = new Schema({
                     type: "largeSlider",
                     content: [
                         {
-                            "desktop": "https://static.vecteezy.com/system/resources/previews/006/828/785/non_2x/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-pink-backgroud-for-banner-market-ecommerce-women-concept-free-vector.jpg",
-                            "mobile": "https://img.pikbest.com/backgrounds/20210618/creative-technology-smart-style-mobile-promotion-banner-template_6021593.jpg!bw700"
+                            desktop: "https://static.vecteezy.com/system/resources/previews/006/828/785/non_2x/paper-art-shopping-online-on-smartphone-and-new-buy-sale-promotion-pink-backgroud-for-banner-market-ecommerce-women-concept-free-vector.jpg",
+                            mobile: "https://img.pikbest.com/backgrounds/20210618/creative-technology-smart-style-mobile-promotion-banner-template_6021593.jpg!bw700"
                         }
                     ]
                 }
@@ -31,6 +34,13 @@ const DispositionSchema = new Schema({
     }
 }, { timestamps: true });
 
+// Middleware para asegurar valores por defecto en `content`
+DispositionItemSchema.pre("save", function (next) {
+    if (this.content === undefined) {
+        this.content = [];
+    }
+    next();
+});
 
 // Modelo para DispositionModel
 const DispositionModel = mongoose.model("Disposition", DispositionSchema);
