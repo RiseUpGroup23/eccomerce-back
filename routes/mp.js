94578a3fb974e927fd2c8054eea6f07d30d6698f
Nicolás Amicone <nicolas@pocimadigital.com>
2025-05-05 13:48:21 -0300

    mp

--- routes/mp.js
+++ routes/mp.js
@@ -9,7 +9,7 @@ const mp = new MercadoPagoConfig({
 const payment = new Payment(mp);
 router.post('/create-payment', async (req, res) => {
   try {
-    const response = await payment.create(req.body);
+    const response = await payment.create({ body: req.body });
 
     res.status(200).json(response);
   } catch (error) {
