cecc00be191cfe903ceaa867ec5b064458e5f5ff
Nicolás Amicone <nicolas@pocimadigital.com>
2025-05-14 13:07:11 -0300

    mejoras

--- models/product/productModel.js
+++ models/product/productModel.js
@@ -26,25 +26,26 @@ const productoSchema = new mongoose.Schema({
             }
         }]
     }],
-    quantity: { type: Number, required: false },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Categoria'}, // Categoría principal
-    subcategory: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Subcategoria',
-        default: null // Permitir que subcategory sea null o un ObjectId de subcategoría
-    },
+    quantity: { type: Number },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Categoria' },
+    subcategory: { type: mongoose.Schema.Types.ObjectId, ref: 'Subcategoria', default: null },
     images: [{ type: String }],
     brand: { type: String },
+    totalStock: {
+        type: Number,
+        default: 0
+    }
 }, {
     timestamps: true,
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
 });
 
-productoSchema.virtual('totalStock').get(function () {
-    return this.variants.reduce((acc, variant) => {
+productoSchema.pre('save', function (next) {
+    this.totalStock = this.variants.reduce((acc, variant) => {
         return acc + variant.stockByPickup.reduce((sum, sp) => sum + sp.quantity, 0);
     }, 0);
+    next();
 });
 
 module.exports = mongoose.model('Producto', productoSchema);
