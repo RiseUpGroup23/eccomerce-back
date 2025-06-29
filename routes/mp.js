82c905f3039826b8ccfc28e0e8adc09e5351bf03
Nicolás Amicone <nicolas@pocimadigital.com>
2025-06-29 19:33:30 -0300

    mp

--- routes/mp.js
+++ routes/mp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { MercadoPagoConfig, Payment } = require('mercadopago');
+const { MercadoPagoConfig, Payment, Preference } = require('mercadopago');
 
 const mp = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN || "APP_USR-2980739470681237-041511-dd3fddadddfccba8c4ebfccfc70d1cd2-229156870"
@@ -26,6 +26,44 @@ router.post('/webhook', async (req, res) => {
   } catch (error) {
     console.error('Error en webhook:', error.message);
     res.sendStatus(500);
+  }
+});
+
+router.post("/create-preference", async (req, res) => {
+  try {
+    const frontOrigin = req.body.origin.endsWith("/") ? req.body.origin.slice(0, -1) : req.body.origin
+    const backUrl = (req.protocol.endsWith("s") ? req.protocol : (req.protocol + "s")) + '://' + req.get('host');
+    console.log("noti url", `${backUrl}/mercadopago/webhook`);
+
+    const body = {
+      items: [
+        {
+          title: req.body.name,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.price),
+          currency_id: "ARS",
+        },
+      ],
+      back_urls: {
+        success: `${frontOrigin}/reserva-confirmada`,
+        failure: `${frontOrigin}/reserva-error`,
+      },
+      auto_return: "approved",
+      notification_url: `${backUrl}/mercadopago/webhook`,
+      metadata: req.body,
+    };
+
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+
+    res.json({
+      id: result.id,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: error.message,
+    });
   }
 });
 
