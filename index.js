6350b4ea6142aba107edef1e05b3021afefec85f
Nicolás Amicone <nicolas@pocimadigital.com>
2025-08-05 14:04:43 -0300

    headers para ios

--- index.js
+++ index.js
@@ -25,13 +25,7 @@ const corsOptions = {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: [
-        'Content-Type',
-        'Authorization',
-        'X-Requested-With',
-        'Accept',
-        'Origin'
-    ]
+    allowedHeaders: '*'
 };
 
 // 💡 Muy importante: habilitar OPTIONS para todas las rutas
